0a7b780556c84d91594a5f80082fbf50e7cb1ad6
Luancss <ventobo123@gmail.com>
2023-09-29 13:55:23 -0300

    CSS Global

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { Header } from './components/Header';
+import './global.css'
+
 function App() {
   return (
     <>
+    <Header/>
       <h1>Hello World</h1>
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header>
+      <strong className={styles.header}>Ignite Feed</strong>
+    </header>
+  );
+}
