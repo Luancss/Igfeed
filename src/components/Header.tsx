ffc87ccb9d1f1b5c58f9f80e0d34e0f4c5937ec8
Luancss <ventobo123@gmail.com>
2023-09-29 14:02:26 -0300

    Componente - Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import styles from "./Header.module.css";
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
   return (
-    <header>
-      <strong className={styles.header}>Ignite Feed</strong>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do ignite" />
     </header>
   );
 }
