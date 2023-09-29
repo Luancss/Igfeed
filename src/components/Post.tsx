bd08cb719d503cdca3eec5ef40ccd8e8b66c4bce
Luancss <ventobo123@gmail.com>
2023-09-29 17:09:20 -0300

    Formulário de comentários

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,10 +5,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/luancss.png"
-          />
+          <img className={styles.avatar} src="https://github.com/luancss.png" />
           <div className={styles.authorInfo}>
             <strong>Luan Carlos</strong>
             <span>Web Developer</span>
@@ -25,12 +22,23 @@ export function Post() {
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
         <p>
           <a href="">#novoprojeto </a>
           <a href="">#nlw </a>
-          <a href="">#rocketseat</a></p>
+          <a href="">#rocketseat</a>
+        </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentario" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
