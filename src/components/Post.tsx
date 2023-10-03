1c0a218b6710583462743f2f5e92e652a30c1769
Luancss <ventobo123@gmail.com>
2023-10-03 17:49:30 -0300

    Estado (useState)

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
+
 export function Post({ author, publishedAt, content }) {
+
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ])
+
   const publishedDateFormatted = new Intl.DateTimeFormat("pt-BR", {
     day: "2-digit",
     month: "long",
     hour: "2-digit",
     minute: "2-digit",
   }).format(publishedAt);
+  
+
+  function handleCreateNewComment(event) {
+    event.preventDefault()
+    
+    setComments([...comments, comments.length + 1])
+  }
 
   return (
     <article className={styles.post}>
@@ -40,7 +55,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentario" />
         <footer>
@@ -48,9 +63,9 @@ export function Post({ author, publishedAt, content }) {
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment/>
+        })}
       </div>
     </article>
   );
